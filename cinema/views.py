8ec2c7158966fa1dfb3d2e2359a41a058e30313b
Anton Bilous <anton.bilosus@gmail.com>
2024-08-24 17:19:21 +0300

    Add PUT method to GenreDetail

--- cinema/views.py
+++ cinema/views.py
@@ -69,3 +69,12 @@ class GenreDetail(APIView):
         genre = self.get_object(pk)
         serializer = GenreSerializer(genre)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
