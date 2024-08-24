9a0287e1541b83e50258268df5279986f46d2772
Anton Bilous <anton.bilosus@gmail.com>
2024-08-24 17:07:28 +0300

    Add POST method to GenreList

--- cinema/views.py
+++ cinema/views.py
@@ -51,3 +51,11 @@ class GenreList(APIView):
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
