519757dc75d9b4264507ca14ccf5afc22fa15d85
Anton Bilous <anton.bilosus@gmail.com>
2024-08-24 17:35:59 +0300

    Add ActorList view to urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail, GenreList, GenreDetail
+from cinema.views import movie_list, movie_detail, GenreList, GenreDetail, ActorList
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
     path("genres/", GenreList.as_view()),
     path("genres/<int:pk>/", GenreDetail.as_view()),
+    path("actors/", ActorList.as_view()),
 ]
 
 app_name = "cinema"
