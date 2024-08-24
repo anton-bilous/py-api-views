b1f1373be1e3773df1405ce1566ecbafa23a517f
Anton Bilous <anton.bilosus@gmail.com>
2024-08-24 17:14:08 +0300

    Add GenreDetail to urls

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail, GenreList
+from cinema.views import movie_list, movie_detail, GenreList, GenreDetail
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
     path("genres/", GenreList.as_view()),
+    path("genres/<int:pk>/", GenreDetail.as_view()),
 ]
 
 app_name = "cinema"
